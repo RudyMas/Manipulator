a0ce744a69c833ce40d1ad9576528536b59e302f
Rudy Mas <rudy.mas@rudymas.be>
2018-03-26 13:58:47 +0200

    Added class CalculatePregnancy Added class DateManipulator

---
+++ src/Manipulator/CalculatePregnancy.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace RudyMas\Manipulator;
+
+/**
+ * Class CalculatePregnancy (PHP version 7.1)
+ * PHP class to calculate weeks/days that someone is pregnant
+ *
+ * @author      Rudy Mas <rudy.mas@rmsoft.be>
+ * @copyright   2018, rmsoft.be (http://www.rmsoft.be/)
+ * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
+ * @version     1.1.0
+ * @package     RudyMas\Manipulator
+ */
+class CalculatePregnancy
+{
+    private $DM;
+
+    /**
+     * CalculatePregnancy constructor.
+     */
+    public function __construct()
+    {
+        $this->DM = new DateManipulator();
+    }
+
+    /**
+     * Calculate the weeks of pregnancy
+     *
+     * @param string $deliveryDate
+     * @param string $referenceDate
+     * @return int
+     */
+    public function calculateWeeks(string $deliveryDate, string $referenceDate): int
+    {
+        $timestampDeliveryDate = $this->DM->convertDateToTimestamp($deliveryDate);
+        $timestampReferenceDate = $this->DM->convertDateToTimestamp($referenceDate);
+        $timestampConceptionDate = $timestampDeliveryDate - (280 * 24 * 60 * 60);
+        return ceil(($timestampReferenceDate - $timestampConceptionDate) / (7 * 24 * 60 * 60));
+    }
+
+    /**
+     * Calculate the days of pregnancy
+     *
+     * @param string $deliveryDate
+     * @param string $referenceDate
+     * @return int
+     */
+    public function calculateDays(string $deliveryDate, string $referenceDate): int
+    {
+        $timestampDeliveryDate = $this->DM->convertDateToTimestamp($deliveryDate);
+        $timestampReferenceDate = $this->DM->convertDateToTimestamp($referenceDate);
+        $timestampConceptionDate = $timestampDeliveryDate - (280 * 24 * 60 * 60);
+        return ceil(($timestampReferenceDate - $timestampConceptionDate) / (24 * 60 * 60));
+    }
+
+    /**
+     * Calculate the weeks of pregnancy by Timestamp
+     *
+     * @param int $timestampDeliveryDate
+     * @param int $timestampReferenceDate
+     * @return int
+     */
+    public function calculateWeeksByTimestamp(int $timestampDeliveryDate, int $timestampReferenceDate): int
+    {
+        $timestampConceptionDate = $timestampDeliveryDate - (280 * 24 * 60 * 60);
+        return ceil(($timestampReferenceDate - $timestampConceptionDate) / (7 * 24 * 60 * 60));
+    }
+
+    /**
+     * Calculate the days of pregnancy by Timestamp
+     *
+     * @param int $timestampDeliveryDate
+     * @param int $timestampReferenceDate
+     * @return int
+     */
+    public function calculateDaysByTimestamp(int $timestampDeliveryDate, int $timestampReferenceDate): int
+    {
+        $timestampConceptionDate = $timestampDeliveryDate - (280 * 24 * 60 * 60);
+        return ceil(($timestampReferenceDate - $timestampConceptionDate) / (24 * 60 * 60));
+    }
+}
+
+/** End of File: CalculatePregnancy.php **/
---
+++ src/Manipulator/DateManipulator.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace RudyMas\Manipulator;
+
+/**
+ * Class DateManipulator (PHP version 7.1)
+ * PHP class to manipulate date/timestamp
+ *
+ * @author      Rudy Mas <rudy.mas@rmsoft.be>
+ * @copyright   2018, rmsoft.be. (http://www.rmsoft.be/)
+ * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
+ * @version     0.1.0
+ * @package     RudyMas\Manipulator
+ */
+class DateManipulator
+{
+    /**
+     * Converts a correct formatted date to a Timestamp
+     *
+     * @param string $date
+     * @return int
+     */
+    public function convertDateToTimestamp(string $date): int
+    {
+        return strtotime($date);
+    }
+
+    /**
+     * @param int $timestamp
+     * @return string
+     */
+    public function convertTimestampToDate(int $timestamp): string
+    {
+        return date('Y-m-d', $timestamp);
+    }
+
+    /**
+     * Converts a Timestamp to a data (yyyy-mm-dd hh:mm:ss)
+     *
+     * @param int $timestamp
+     * @return string
+     */
+    public function convertTimestampToDateTime(int $timestamp): string
+    {
+        return date('Y-m-d H:i:s', $timestamp);
+    }
+}
+
+/** End of File: DateManipulator.php **/
